69245a6daa61615607e0421c399c176210a2b4ad
DotDev262 <dotdev262@gmail.com>
2026-03-19 09:58:33 +0530

    fix: add ~/.local/share to XDG_DATA_DIRS for Hyprland desktop files

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, zen-browser, username, homeDirectory, ... }:
+{ config, lib, pkgs, zen-browser, username, homeDirectory, ... }:
 
 {
   imports = [
@@ -16,14 +16,30 @@
   home.homeDirectory = homeDirectory;
   home.stateVersion = "25.11";
 
+  targets.genericLinux.enable = true;
+
+  xdg.enable = true;
+
   home.sessionVariables = {
     ANI_CLI_PLAYER = "mpv";
     ANI_CLI_SKIP_INTRO = "1";
+    XDG_DATA_DIRS = lib.mkForce (lib.concatStringsSep ":" [
+      "${homeDirectory}/.local/share"
+      "${homeDirectory}/.local/state/home-manager/profiles/1/share"
+      "${homeDirectory}/.nix-profile/share"
+      "/usr/local/share"
+      "/usr/share"
+    ]);
   };
 
   programs.home-manager.enable = true;
 
   home.sessionPath = [ "$HOME/.local/bin" ];
+
+  # Ensure Hyprland/End-4 picks up Home Manager applications
+  home.file.".config/hypr/custom/env.conf".text = ''
+    env = XDG_DATA_DIRS,"${homeDirectory}/.local/share:${homeDirectory}/.local/state/home-manager/profiles/1/share:${homeDirectory}/.nix-profile/share:$XDG_DATA_DIRS"
+  '';
 
   programs.fish = {
     enable = true;
