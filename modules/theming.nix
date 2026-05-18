7642914c0be7db190279fc9552fdd3a9ce0d96d0
DotDev262 <dotdev262@gmail.com>
2026-05-18 15:55:24 +0530

    disable catppuccin vscode profiles (pnpmDeps build failure)

--- modules/theming.nix
+++ modules/theming.nix
@@ -5,12 +5,12 @@
     enable = true;
     flavor = "mocha";
     helix.enable = true;
-    vscode.profiles = {
-      default.enable = true;
-      python.enable = true;
-      java.enable = true;
-      c-lex.enable = true;
-      typst.enable = true;
-    };
+    # vscode.profiles = {
+    #   default.enable = true;
+    #   python.enable = true;
+    #   java.enable = true;
+    #   c-lex.enable = true;
+    #   typst.enable = true;
+    # };
   };
 }
